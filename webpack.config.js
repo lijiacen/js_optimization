b7da22d34ab24d710d35e01d2c5943d271cb6c7a
lijiacen <50915968+lijiacen@users.noreply.github.com>
2020-08-26 23:22:12 +0800

    react，webpack，口岸

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  // devServer: {
+  //   port: 3000
+  // },
+  entry: {
+    app: "./src/index.jsx"
+  },
+  output: {
+    path: `${__dirname}/build`,
+    filename: "[name].[hash].bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: "babel-loader",
+      include: /src/,
+      exclude: /node_modules/,
+      query: {
+        presets: ["@babel/preset-react"]
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "template.html"
+    })
+  ]
+};
